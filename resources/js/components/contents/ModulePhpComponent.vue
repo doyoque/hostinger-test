8e2d7dcc6cfb6420b1e8efbbc7d68a69a675a9be
yoga <abiyoga1335@gmail.com>
2020-06-04 23:12:53 +0700

    webpack sourceMaps, php modules content

--- resources/js/components/contents/ModulePhpComponent.vue
+++ resources/js/components/contents/ModulePhpComponent.vue
@@ -3,6 +3,15 @@
     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" id="1">
       <h4 class="content-title text-center">Modul Lengkap untuk Menjalankan Aplikasi PHP Anda</h4>
     </div>
-    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" id="2"></div>
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center" id="2">
+      <button class="btn btn-outline-dark btn-lg btn-php-modules">Selengkapnya</button>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  mounted() {
+    console.log("Module php mounted");
+  }
+};
+</script>
